7d7d54c0134ee304f57d69941491bce66036a39a
Juan-Bogota <1213@holbertonschool.com>
2020-04-02 17:25:47 -0500

    [Feature]DBStorage - States and Cities: storage engine and use SQLAlchemy

--- models/state.py
+++ models/state.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    @property
+    def cities(self):
+        """ Returns the list of City
+        """
+        the_cities = []
+        for c in models.storage.all(City).values():
+            if c.state_id == self.id:
+                the_cities.append(c)
+        return the_cities
