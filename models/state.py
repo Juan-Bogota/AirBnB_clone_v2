ca5bb21d533ff596169447c5872eed8e66dc277c
juan-skill <1294@holbertonschool.com>
2020-04-02 22:27:21 +0000

    add change state.py

--- models/state.py
+++ models/state.py
@@ -1,11 +1,46 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+
+import os
+
+from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class State(BaseModel):
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class State(BaseModel, Base):
     """This is the class for State
-    Attributes:
-        name: input name
     """
-    name = ""
+
+    if os.environ['HBNB_TYPE_STORAGE'] == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City",
+                              cascade="all, delete-orphan",
+                              backref="state")
+    else:
+        cities = ""
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+    if os.environ['HBNB_TYPE_STORAGE'] != "db":
+
+        @property
+        def cities(self):
+            '''
+            FileStorage to return City
+            instances with state_id == current State.id
+            '''
+            list_cities = []
+            for city_instance in models.storage.all(City).values():
+
+                if city_instance.state_id == self.id:
+                    list_cities.append(city_instance)
+
+                    return list_cities
