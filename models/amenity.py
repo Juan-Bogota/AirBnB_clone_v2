ba35472dc7e7f27470b0eba45b21a9d7ad395ede
Juan-Bogota <1213@holbertonschool.com>
2020-04-03 13:19:01 -0500

    [Feature] Fix errors filestorage

--- models/amenity.py
+++ models/amenity.py
@@ -12,8 +12,8 @@ class Amenity(BaseModel, Base):
     Attributes:
         name: input name
     """
+    __tablename__ = "amenities"
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity)
     else:
--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from os import getenv
 
 
 class City(BaseModel, Base):
@@ -13,6 +14,11 @@ class City(BaseModel, Base):
         name: input name
     """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+    else:
+        name = ""
+        state_id = ""
