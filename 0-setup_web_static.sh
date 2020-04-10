edd8a0c371293aefb4bcd2cb45a2472bf428444e
Juan-Bogota <1213@holbertonschool.com>
2020-04-10 15:08:17 -0500

    [Feature] Add file Setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,8 @@
 
 sudo apt-get update
 sudo apt-get install nginx -y
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
@@ -10,6 +12,6 @@ sudo touch /data/web_static/releases/test/index.html
 echo "My Web Page Holberton" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-new="server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n"
+new="server_name _;\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n"
 sudo sed -i "s/server_name _;/$new/" /etc/nginx/sites-available/default
 sudo service nginx restart
