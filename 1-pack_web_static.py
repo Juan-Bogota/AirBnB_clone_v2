9ca53e222a02681677183c30cfde4328d713d09b
Juan-Bogota <1213@holbertonschool.com>
2020-04-11 04:14:50 +0000

    [Feature] Pack web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,14 +5,15 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
-     try:
-          formato = "%Y%m%d%H%M%S"
-          date_now = datetime.now()
-          created_at = date_now.strftime(formato)
-          local("mkdir -p versions")
-          file_tgz = "web_static_{}.tgz".format(created_at)
-          local("tar -cvzf versions/{} web_static".format(file_tgz))
-          return file_tgz
-     except:
-          return None
+    try:
+        formato = "%Y%m%d%H%M%S"
+        date_now = datetime.now()
+        created_at = date_now.strftime(formato)
+        local("mkdir -p versions")
+        file_tgz = "web_static_{}.tgz".format(created_at)
+        local("tar -cvzf versions/{} web_static".format(file_tgz))
+        return file_tgz
+    except:
+        return None
