6aef03f1c56a8cd4b24930d8b514f3ddb7fa921f
Juan-Bogota <1213@holbertonschool.com>
2020-04-11 23:00:05 -0500

    [Feature] Setup WEb Static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,6 @@
 # Prepare your web serversr with puppet.
 
 exec { 'Deploy server':
-  command  => 'sudo apt-get -y update && sudo apt-get install nginx -y && sudo mkdir -p /data/web_static/releases/ && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && sudo touch /data/web_static/releases/test/index.html && echo "My Web Page Holberton" > /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data && str="server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}\n" && sudo sed -i "s/server_name _;/$str/" /etc/nginx/sites-available/default && sudo service nginx restart',
+  command  => 'sudo apt-get update -y && sudo apt-get install nginx -y && sudo mkdir -p /data/ && sudo mkdir -p /data/web_static/ && sudo mkdir -p /data/web_static/releases/ && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && sudo touch /data/web_static/releases/test/index.html && echo "My Web Page Holberton" | sudo tee /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i "38i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default && sudo service nginx restart',
   provider => 'shell',
 }
